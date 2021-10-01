eabdbbe028e7fe47366bf424019c86c368c74328
Sina Zamani <sinazamani920@gmail.com>
2021-10-01 12:15:41 +0330

    Implement Domain Model Classes

---
+++ src/crawler/domain/model.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from datetime import datetime
+from typing import List, Optional, Union
+from uuid import UUID
+
+from src.crawler.domain import events
+
+
+class Product:
+    def __init__(
+        self,
+        name: str,
+        description: Optional[str] = None,
+        type_id: int = None,
+        available: bool = True,
+        express_delivery: bool = True,
+        rating: Optional[float] = None,
+        categories = List[str],
+        attributes = List[str]
+    ):
+        self.name = name
+        self.description = description
+        self.type_id = type_id
+        self.available = available
+        self.express_delivery = express_delivery
+        self.rating = rating
+        self.categories = categories
+        self.attributes = attributes
+        self.events: List[events.Event] = []
+
+
+
+
+@dataclass(unsafe_hash=True)
+class Log:
+    product_id: int
+    time_stamp: datetime
